0bd024eb45379732f6bcd276e8a1469c5dfbe63a
SEANGLAY <liseanglay@gmail.com>
2023-03-25 13:27:07 +0700

    add to route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\AuthenticationController;
 use App\Http\Controllers\API\CategoryController;
+use App\Models\SubCategory;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,10 +22,16 @@ Route::post('login', [AuthenticationController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::prefix('categories')->group(function () {
-        Route::get('',[CategoryController::class,'index']);
+        Route::get('', [CategoryController::class, 'index']);
         Route::post('', [CategoryController::class, 'store']);
-        Route::put('{id}',[CategoryController::class,'edit']);
-
+        Route::put('{id}', [CategoryController::class, 'edit']);
+        Route::delete('{id}', [CategoryController::class, 'destroy']);
+    });
+    Route::prefix('sub_categories')->group(function () {
+        Route::get('', [SubCategory::class, 'index']);
+        Route::post('', [SubCategory::class, 'store']);
+        Route::put('{id}', [SubCategory::class, 'edit']);
+        Route::delete('{id}', [SubCategory::class, 'destroy']);
     });
 });
 
